e0b3285fdf9c246404aed9f29885c0f07c79973c
Nina Tymińska <nina.tyminska@k2.pl>
2020-05-10 21:40:37 +0200

    feat(repo): project 1 moved for deploy

--- project-1-it-spa/models/treatment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const treatmentSchema = new mongoose.Schema({
-    name: String,
-    area: String,
-    time: Number,
-    price: Number,
-});
-module.exports = mongoose.model('Treatment', treatmentSchema);
